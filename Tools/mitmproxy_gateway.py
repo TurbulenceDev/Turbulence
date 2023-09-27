1690046a8e1ccbb27e3ab337d31b844ae7970478
Exp <17885980+exp111@users.noreply.github.com>
2023-09-27 12:33:51 +0200

    support parallel gateways

--- Tools/mitmproxy_gateway.py
+++ Tools/mitmproxy_gateway.py
@@ -8,16 +8,16 @@ import zlib, json
 class Gateway:
     def __init__(self):
         self.ZLIB_SUFFIX = b'\x00\x00\xff\xff'
-        self.buffer = bytearray()
-        self.inflator = zlib.decompressobj()        
+        self.buffers = {}
+        self.inflators = {}
 
     def websocket_message(self, flow):
         # only check discord gateway msgs 
         if not "gateway.discord.gg" in flow.request.host:
             return
         
-        #TODO: as we listen to all requests there is a possibility of having two seperate gateway ws, so we should probably assign a inflator for each ws
         message = flow.websocket.messages[-1]
+        id = flow.id
         msg = message.content
 
         # was the message sent from the client or server?
@@ -26,23 +26,27 @@ class Gateway:
             msg = json.loads(msg)
             # print stuff about msgs
             if msg["op"] == 0: # t only != null, if op == 0
-                ctx.log.info(f"Client -> Server: op: 0, t: {msg['t']}")
+                ctx.log.info(f"[{id}] Client -> Server: op: 0, t: {msg['t']}")
             else:
-                ctx.log.info(f"Client -> Server: op: {msg['op']}")
+                ctx.log.info(f"[{id}] Client -> Server: op: {msg['op']}")
             ctx.log.info(msg["d"])
         else:
+            # init buffer+inflator for this websocket
+            if not id in self.buffers:
+                self.buffers[id] = bytearray()
+                self.inflators[id] = zlib.decompressobj()
             # zlib decompress (inflate) the msg
-            self.buffer.extend(msg)
+            self.buffers[id].extend(msg)
             if len(msg) < 4 or msg[-4:] != self.ZLIB_SUFFIX:
-                ctx.log.info(f"invalid msg: {msg!r}")
+                ctx.log.info(f"[{id}] invalid msg: {msg!r}")
                 return
 
-            msg = self.inflator.decompress(self.buffer)
-            self.buffer = bytearray()
+            msg = self.inflators[id].decompress(self.buffers[id])
+            self.buffers[id] = bytearray()
             # parse as json
             msg = json.loads(msg)
             # print info about msg
-            ctx.log.info(f"Server -> Client: op: {msg['op']}, t: {msg['t']}")
+            ctx.log.info(f"[{id}] Server -> Client: op: {msg['op']}, t: {msg['t']}")
             # ignore the long msgs we dont want anyway for now
             if msg["t"] != "READY" \
                 and msg["t"] != "READY_SUPPLEMENTAL":
