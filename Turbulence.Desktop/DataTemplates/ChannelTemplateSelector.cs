f725329f7b9b98d7af31f37a3fb5e6dabf6e29dd
Exp <17885980+exp111@users.noreply.github.com>
2023-09-28 17:01:21 +0200

    render channel types differently

---
+++ Turbulence.Desktop/DataTemplates/ChannelTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls.Templates;
+using Avalonia.Controls;
+using Avalonia.Metadata;
+using Turbulence.Discord.Models.DiscordChannel;
+
+namespace Turbulence.Desktop.DataTemplates;
+
+public class ChannelTemplateSelector : IDataTemplate
+{
+    public bool SupportsRecycling => false;
+    [Content]
+    public Dictionary<string, IDataTemplate> Templates { get; } = new Dictionary<string, IDataTemplate>();
+
+    public bool Match(object? data) => data is Channel;
+
+    Control? ITemplate<object?, Control?>.Build(object? param)
+    {
+        var channel = (Channel)param!;
+        if (!Templates.TryGetValue(channel.Type.ToString(), out var template))
+            return Templates["unknown"].Build(param);
+        return template.Build(param);
+    }
+}
