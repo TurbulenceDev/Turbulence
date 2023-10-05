5ea078992a453ec919d5ec647135074b9ed1dd6a
Exp <17885980+exp111@users.noreply.github.com>
2023-10-05 11:00:54 +0200

    channel list mock data

---
+++ Turbulence.Core/ViewModels/Design/DesignChannelListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Turbulence.Discord.Models.DiscordChannel;
+
+namespace Turbulence.Core.ViewModels.Design;
+
+public class DesignChannelListViewModel : ChannelListViewModel
+{
+    public DesignChannelListViewModel()
+    {
+        Channels.AddRange(new List<Channel>()
+        {
+            new Channel()
+            {
+                Id = new(0),
+                Type = ChannelType.GUILD_CATEGORY,
+                Name = "Category"
+            },
+            new Channel()
+            {
+                Id = new(1),
+                Type = ChannelType.GUILD_TEXT,
+                Name = "Text"
+            },
+            new Channel()
+            {
+                Id = new(2),
+                Type = ChannelType.GUILD_VOICE,
+                Name = "Voice"
+            }
+        });
+    }
+}
