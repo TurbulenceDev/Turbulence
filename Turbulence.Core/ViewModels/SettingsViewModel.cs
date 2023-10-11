c4a78caf5b40980092e2cf81cc5c4241d38c5ada
Exp <17885980+exp111@users.noreply.github.com>
2023-10-11 14:48:07 +0200

    non functional set token

---
+++ Turbulence.Core/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Configuration;
+using Turbulence.Discord;
+
+namespace Turbulence.Core.ViewModels;
+
+public partial class SettingsViewModel : ViewModelBase
+{
+    //TODO: save current user?
+
+    [RelayCommand]
+    private void SetToken(string token)
+    {
+        //TODO: run in service?
+        //Messenger.Send(new SetTokenMessage(token));
+        var config = new ConfigurationManager().AddUserSecrets<Client>().Build();
+        config["token"] = token;
+        //TODO: actually save it lmao
+    }
+}
--- Turbulence.Desktop/Views/Settings/AccountSettingsView.axaml.cs
+++ Turbulence.Desktop/Views/Settings/AccountSettingsView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Turbulence.Core.ViewModels;
 
 namespace Turbulence.Desktop.Views.Settings;
 
@@ -7,5 +9,19 @@ public partial class AccountSettingsView : UserControl
     public AccountSettingsView()
     {
         InitializeComponent();
+    }
+
+    public void OnSetToken(object? sender, RoutedEventArgs? e)
+    {
+        ((SettingsViewModel)DataContext!).SetTokenCommand.Execute(Token.Text);
+        Token.Text = "";
+    }
+
+    public void OnTokenShow(object? sender, RoutedEventArgs? e)
+    {
+        if (sender is CheckBox check)
+        {
+            Token.RevealPassword = check.IsChecked ?? false;
+        }
     }
 }
