30b47dd94aef036ec0af81134754c2c114bd0fe9
Exp <17885980+exp111@users.noreply.github.com>
2023-10-24 11:32:16 +0200

    fix tgui missing services

--- Turbulence.TGUI/Program.cs
+++ Turbulence.TGUI/Program.cs
@@ -2,10 +2,13 @@
 using Microsoft.Extensions.DependencyInjection;
 using Terminal.Gui;
 using Turbulence.Discord;
+using Turbulence.Discord.Services;
 using Turbulence.TGUI;
 
 var provider = new ServiceCollection()
     .AddSingleton<IPlatformClient, Client>()
+    .AddSingleton<ICache, Cache>()
+    .AddSingleton<ITypingStorage, TypingStorage>()
     .BuildServiceProvider();
 
 Ioc.Default.ConfigureServices(provider);
