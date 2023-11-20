4079d57165a8026c2e2fc35a9420d5a7179295f7
Exp <17885980+exp111@users.noreply.github.com>
2023-11-20 15:01:01 +0100

    test asserts

--- Turbulence.Discord.Test/MessageParser/LexerTests.cs
+++ Turbulence.Discord.Test/MessageParser/LexerTests.cs
@@ -12,12 +12,32 @@ public class LexerTests
     [Test]
     public void Test1()
     {
-        var tokens = Lexer.Lex("hello, **bold**");
-        Assert.IsNotNull(tokens);
-        Assert.IsNotEmpty(tokens);
+        var tokens = Lexer.Lex("hello, **world**");
+        Assert.That(tokens, Is.Not.Null);
+        Assert.That(tokens, Is.Not.Empty);
         foreach (var token in tokens)
         {
             Console.WriteLine(token);
+        }
+        var should = new Token[]
+        {
+            new Token(TokenType.TEXT_INLINE, "hello, "),
+            new Token(TokenType.STAR, "*"),
+            new Token(TokenType.STAR, "*"),
+            new Token(TokenType.TEXT_INLINE, "world"),
+            new Token(TokenType.STAR, "*"),
+            new Token(TokenType.STAR, "*"),
+        };
+        Assert.That(tokens.Count(), Is.EqualTo(should.Length));
+        for (var i = 0; i < should.Length; i++)
+        {
+            var actual = tokens.ElementAt(i);
+            var expected = should[i];
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Type, Is.EqualTo(expected.Type));
+                Assert.That(actual.Value, Is.EqualTo(expected.Value));
+            });
         }
     }
 }
--- Turbulence.Discord.Test/MessageParser/ParserTests.cs
+++ Turbulence.Discord.Test/MessageParser/ParserTests.cs
@@ -17,13 +17,14 @@ public class ParserTests
             new Token(TokenType.TEXT_INLINE, "hello, "),
             new Token(TokenType.STAR, "*"),
             new Token(TokenType.STAR, "*"),
-            new Token(TokenType.TEXT_INLINE, "bold"),
+            new Token(TokenType.TEXT_INLINE, "world"),
             new Token(TokenType.STAR, "*"),
             new Token(TokenType.STAR, "*"),
         };
         var parts = Parser.ParseTokens(tokens);
-        Assert.IsNotEmpty(parts);
-        void print(Node node, int depth = 0)
+        Assert.That(parts, Is.Not.Empty);
+
+        static void print(Node node, int depth = 0)
         {
             Console.WriteLine($"{new string('-', depth)}{node}");
             if (node.Children != null)
@@ -37,6 +38,50 @@ public class ParserTests
         foreach (var part in parts)
         {
             print(part);
+        }
+
+        var should = new Node[]
+        {
+            new(NodeType.TEXT, text: "hello, "),
+            new(NodeType.BOLD, children: new List<Node>(){
+                new(NodeType.TEXT, text: "world")
+            }),
+        };
+        // asserts that two nodes (and its children) are the same
+        static void assert(Node should, Node actual)
+        {
+            // Properties
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Type, Is.EqualTo(should.Type));
+                Assert.That(actual.Text, Is.EqualTo(should.Text));
+                Assert.That(actual.Id, Is.EqualTo(should.Id));
+                Assert.That(actual.Emoji, Is.EqualTo(should.Emoji));
+                Assert.That(actual.CodeLanguage, Is.EqualTo(should.CodeLanguage));
+                Assert.That(actual.Url, Is.EqualTo(should.Url));
+            });
+            // Children
+            if (actual.Children == null)
+                Assert.That(should.Children, Is.Null);
+            else
+            {
+                Assert.That(should.Children, Is.Not.Null);
+                Assert.That(actual.Children, Has.Count.EqualTo(should.Children!.Count));
+                for (var i = 0; i < actual.Children.Count; i++)
+                {
+                    var a = actual.Children[i];
+                    var s = should.Children![i];
+                    assert(s, a);
+                }
+            }
+        }
+
+        Assert.That(parts, Has.Count.EqualTo(should.Length));
+        for (var i = 0; i < should.Length; i++)
+        {
+            var a = parts[i];
+            var s = should[i];
+            assert(s, a);
         }
     }
 }
