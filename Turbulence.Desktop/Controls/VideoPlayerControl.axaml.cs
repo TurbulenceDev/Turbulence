732bf83ebc46c0d636e61ce75b580f8f89354252
Paul <ritter.paul1@googlemail.com>
2023-12-02 20:57:07 +0100

    abolish VideoPlayerControlModel.cs

--- Turbulence.Desktop/Controls/VideoPlayerControl.axaml.cs
+++ Turbulence.Desktop/Controls/VideoPlayerControl.axaml.cs
@@ -3,7 +3,10 @@ using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Reactive;
+using CommunityToolkit.Mvvm.DependencyInjection;
 using LibVLCSharp.Avalonia.Unofficial;
+using LibVLCSharp.Shared;
 
 namespace Turbulence.Desktop.Controls;
 
@@ -18,32 +21,57 @@ public partial class VideoPlayerControl : UserControl
             false);
 
     private VideoView? _videoViewer;
-    public VideoPlayerControlModel viewModel;
+    
+    private readonly LibVLC _libVLC = Ioc.Default.GetService<LibVLC>()!;
+    public MediaPlayer? MediaPlayer;
+    private Uri? _media;
 
     public Uri? Media
     {
-        get => viewModel.Media;
-        set => viewModel.Media = value;
+        get => _media;
+        set
+        {
+            _media = value;
+            if (_media == null)
+            {
+                MediaPlayer!.Media = null;
+            }
+            else
+            {
+                using var media = new Media(_libVLC, _media);
+                MediaPlayer!.Media = media;
+                media.Dispose();
+            }
+        }
     }
 
     public VideoPlayerControl()
     {
         InitializeComponent();
 
-        viewModel = new VideoPlayerControlModel();
+        if (!Design.IsDesignMode)
+        {
+            MediaPlayer = new MediaPlayer(_libVLC);
+        }
 
         _videoViewer = this.Get<VideoView>("VideoViewer");
         
         _videoViewer.Loaded += VideoViewerLoaded;
+        _videoViewer.GetObservable(IsVisibleProperty).Subscribe(new AnonymousObserver<bool>(b =>
+            {
+                if (b) MediaPlayer?.Play();
+                else MediaPlayer?.Pause();
+            }
+        ));
     }
 
     private void VideoViewerLoaded(object? sender, RoutedEventArgs e)
     {
-        if (_videoViewer != null && viewModel.MediaPlayer != null)
+        if (_videoViewer != null && MediaPlayer != null)
         {
-            _videoViewer.MediaPlayer = viewModel.MediaPlayer;
+            _videoViewer.MediaPlayer = MediaPlayer;
             _videoViewer.MediaPlayer.Hwnd = _videoViewer.hndl.Handle;
-            viewModel.Play();
+            MediaPlayer?.Play();
         }
     }
 
