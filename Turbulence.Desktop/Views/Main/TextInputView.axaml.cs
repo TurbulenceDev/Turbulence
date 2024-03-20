a44a3ceec3703a10f6566b82cced67a0f4c1c895
Exp <17885980+exp111@users.noreply.github.com>
2024-03-20 11:35:20 +0100

    allow multi line msgs with shift+enter

--- Turbulence.Desktop/Views/Main/TextInputView.axaml.cs
+++ Turbulence.Desktop/Views/Main/TextInputView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Turbulence.Core.ViewModels.Design;
 
 namespace Turbulence.Desktop.Views.Main;
@@ -8,10 +10,23 @@ public partial class TextInputView : UserControl
     public TextInputView()
     {
         InitializeComponent();
+        TextInput.AddHandler(KeyDownEvent, OnPreviewKeyDown, RoutingStrategies.Tunnel);
         if (Design.IsDesignMode)
         {
             // Workaround to fix design data context getting overwritten
             DataContext = new DesignTextInputViewModel();
+        }
+    }
+    
+    private void OnPreviewKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter && !e.KeyModifiers.HasFlag(KeyModifiers.Shift))
+        {
+            var command = SendButton.GetValue(Button.CommandProperty);
+            if (command != null && command.CanExecute(null))
+                command.Execute(null);
+
+            e.Handled = true;
         }
     }
 }
